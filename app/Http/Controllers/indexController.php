db3b356a94499249798b3374966aa22eeeb184e7
davidtadeovargas <coritocorito@hotmail.com>
2018-11-29 21:10:21 -0600

    modicación de archivo index
    quitando variable dominio
    ya que se crearon rutas nuevas

--- app/Http/Controllers/indexController.php
+++ app/Http/Controllers/indexController.php
@@ -8,13 +8,12 @@ class indexController extends Controller
 {
     public function index()
     {
-    	$dominio = \tiendaMusical\configuraciones::getDominio();
         //----- < para el menu---------
-        $lineas = \tiendaMusical\lineas::getLinea();        
+        $lineas = \tiendaMusical\lineas::getLinea();
         $categorias = \tiendaMusical\categorias::getCategorias();
         $productos = \tiendaMusical\productos::getProductos();
         //----- para el menu >---------
         //dd($productos);
-    	return view('index',compact('dominio','lineas','categorias','marcas','productos'));
+    	return view('index',compact('lineas','categorias','marcas','productos'));
     }
 }
