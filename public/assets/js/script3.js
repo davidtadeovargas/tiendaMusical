fdc930a058e41ba360d48f5b0ee1e4bdb3a7cd6b
davidtadeovargas <coritocorito@hotmail.com>
2018-12-01 23:03:20 -0600

    deshabilitar script temporal

--- public/assets/js/script3.js
+++ public/assets/js/script3.js
@@ -1,17 +1,18 @@
-jQuery(document).on('click','.pagination a',function(e){
+/*jQuery(document).on('click','.pagination a',function(e){
 
 	e.preventDefault(); //prevenimos algun evento
 	var page = jQuery(this).attr('href').split('page=')[1];//leemos el contenido del atributo href
 	var route = window.location;//obtener url actual
-	//console.log(page+" "+route);
+	console.log(page+" "+route);
 	jQuery.ajax({
 		url : route,
 		data : {page : page},
 		type : 'GET',
 		dataType : 'json',
 		success: function(data){
+			console.log(data);
 			jQuery(".productos").html(data);
 		}
 	});
 	
-});
+});*/
